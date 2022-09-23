03ac531afb64039ddfd0dd598765699309b72b93
Alejandro Amenabar <alegymnas@gmail.com>
2022-09-23 16:32:20 -0300

    avance 1

---
+++ tareaClase4/cuenta.cs
@@ -0,0 +1,57 @@
+public enum TipoDeExtraccion
+{
+    CajeroHumano=1,
+    CajeroAutomatico=2
+}
+public abstract class Cuenta
+{
+    public abstract void insercion(int monto);
+    public abstract void extraccion(int monto, TipoDeExtraccion tipo);
+
+}
+
+public class CuentaCorrientePesos : Cuenta
+{
+    public float fondos;
+    public override void insercion(int monto) 
+    {
+        // Do something else.
+    }
+    public override void extraccion(int monto, TipoDeExtraccion tipo)
+    {
+        if (tipo==TipoDeExtraccion.CajeroAutomatico)
+        {
+            if (monto<20000 && monto-fondos<-5000)
+            {
+                System.Console.WriteLine("todo ok");
+            }else{
+
+                System.Console.WriteLine("todo mal");
+            }
+        }
+    }
+}
+public class CuentaCorrienteDolares : Cuenta
+{
+    public override void insercion(int monto) 
+    {
+        // Do something else.
+    }
+    public override void extraccion(int monto, TipoDeExtraccion tipo)
+    {
+        //pepe
+    }
+}
+
+public class cajaAhorroPesos : Cuenta
+{
+    public override void insercion(int monto) 
+    {
+        // Do something else.
+    }
+    public override void extraccion(int monto, TipoDeExtraccion tipo)
+    {
+        //pepe
+    }
+}
+
